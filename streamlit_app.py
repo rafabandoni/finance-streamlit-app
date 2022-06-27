389b82170c295314fcf2f5fa57e2396951a2355f
Rafael Bandoni <50203238+rafabandoni@users.noreply.github.com>
2022-06-27 14:41:36 -0300

    Update streamlit_app.py
    Added first data structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
 
+st.title('Meu app de finanças')
+st.header('Minha tabela financeira:')
+
+data = pd.read_csv('https://raw.githubusercontent.com/rafabandoni/finance-streamlit-app/main/fake-data.csv')
+st.dataframe(data)
