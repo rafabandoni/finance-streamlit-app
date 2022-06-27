29824df7d2b6e2048a5a1aec3a87afcf3c0bdaf1
rafabandoni <rafagbandoni@gmail.com>
2022-06-27 14:52:55 -0300

    making some tests with dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,11 @@ st.title('Meu app de finanças')
 st.header('Minha tabela financeira:')
 
 data = pd.read_csv('https://raw.githubusercontent.com/rafabandoni/finance-streamlit-app/main/fake-data.csv')
+data.set_index(['data'], inplace=True)
+st.dataframe(data)
+
+data['gastos_fixos'] = data['agua'] + data['luz'] + data['gás'] + data['internet'] + data['aluguel'] + data['condominio']
+data['gastos_variaveis'] = data['cartao_credito']
+data['sobra_mes'] = data['gastos_fixos'] + data['gastos_variaveis'] - data['salario']
+
 st.dataframe(data)
